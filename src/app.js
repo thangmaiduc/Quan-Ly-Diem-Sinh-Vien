0dc128ea72669930592d71d9816dfd3b5280dc12
thangmaiduc <thangmaiduc00@gmail.com>
2021-06-03 13:37:59 +0700

    test

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // set locals, only providing error in 
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
