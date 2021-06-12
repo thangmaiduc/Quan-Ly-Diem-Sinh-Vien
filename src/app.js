19d5b80137ef5a00fbad4d5b54bc1e8df45344a8
thangmaiduc <thangmaiduc00@gmail.com>
2021-06-12 14:18:02 +0700

    fix cors 2

--- src/app.js
+++ src/app.js
@@ -88,10 +88,11 @@ app.use(function(err, req, res, next) {
 });
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
-});
+})
 
 userModel.hasOne(studentModel, {foreignKey: 'userId', onDelete: 'CASCADE'});
 userModel.hasOne(teacherModel, {foreignKey: 'userId', onDelete: 'CASCADE'});
